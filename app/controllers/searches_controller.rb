c086a5c840eab8f23b6c47e1917d1269b8e69c1c
Kenny Leung <kleung105@gmail.com>
2015-03-10 22:55:57 -0400

    Improved search, DRY operation on index actions

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -5,19 +5,38 @@ class SearchesController < ApplicationController
 
     search_term = '%' + @query + '%'
 
-    if @type == 'Articles'
-      @results = Article.where('display LIKE ?', search_term)
-    elsif @type == 'Events'
-      @results = Event.where('display LIKE ?', search_term)
-    elsif @type == 'Concepts'
-      @results = Concept.where('display LIKE ?', search_term)
-    elsif @type == 'Opinions'
-      @results = Opinion.where('display LIKE ?', search_term)
-    end
+    @articles = Article.where('display LIKE ?', search_term)
+    @events = Event.where('display LIKE ?', search_term)
+    @concepts = Concept.where('display LIKE ?', search_term)
+    @opinions = Opinion.where('display LIKE ?', search_term)
+
+    @@articles = @articles
+    @@events = @events
+    @@concepts = @concepts
+    @@opinions = @opinions
+    @@query = @query
+
+
+    # if @type == 'Articles'
+    #   @results = Article.where('display LIKE ?', search_term)
+    # elsif @type == 'Events'
+    #   @results = Event.where('display LIKE ?', search_term)
+    # elsif @type == 'Concepts'
+    #   @results = Concept.where('display LIKE ?', search_term)
+    # elsif @type == 'Opinions'
+    #   @results = Opinion.where('display LIKE ?', search_term)
+    # end
 
     render :index
   end
 
-  private 
+  def index
+    @articles = @@articles
+    @events = @@events
+    @concepts = @@concepts
+    @opinions = @@opinions
+    @query = @@query
+  end
+  
 
 end
