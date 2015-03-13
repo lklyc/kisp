90a4916145e1bee9867107a1545ee2ce0b949488
Kenny Leung <kleung105@gmail.com>
2015-03-12 22:58:03 -0400

    auto login after registration, brought back home page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
 
     if @user.save
       flash[:success] = "Registration Complete."
+      session[:user_id] = @user.id
       redirect_to root_path
     else
       render :new
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  root 'articles#index'
+  root 'pages#home'
 
   get '/home', to: 'pages#home'
 
